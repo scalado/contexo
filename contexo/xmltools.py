b6ab3d6dabb8ce2eb4c654fa6cfdfccc1c9ff101
michalp <michalp@7223f284-c723-0410-a46e-d6e5c43f17d6>
2009-09-30 21:32:18 +0000

    pretty xml print
    git-svn-id: https://svn.scalado.dom/contexo@313 7223f284-c723-0410-a46e-d6e5c43f17d6

--- contexo/xmltools.py
+++ contexo/xmltools.py
@@ -3,38 +3,50 @@ from xml.sax import saxutils
 from xml.sax.saxutils import XMLGenerator
 from xml.sax.xmlreader import AttributesImpl
 
-#TODO:remove this file
+#pretty formating xml
 
 class XMLGenerator:
     def __init__(self, output):
         self.output = output
+        self.level = 0
+        self.previous_was_end = False
         self.xmlgenerator = saxutils.XMLGenerator(output,'windows-1252');
         self.xmlgenerator.startDocument()
+        self.xmlgenerator.characters('\n')
         #output.write ('<?xml version="1.0" encoding = "Windows-1252"?>\n')
 
     def startElement (self, tag, attributes = {}):
+        if self.previous_was_end:
+            pass
+            #self.xmlgenerator.characters('\n')
+        self.xmlgenerator.characters(multiplystring('\t', self.level))
         self.xmlgenerator.startElement(tag,  attributes)
-#        self.output.write ('<'+tag)
-#        if attributes != None:
-#            self.output.write ('\n')
-#            for a in attributes:
-#                self.output.write (a[0] + '="' + a[1] + '"\n')
-#        self.output.write ('>\n')
+        self.level += 1
+        self.xmlgenerator.characters('\n')
+        self.previous_was_end = False
 
     def endElement (self, tag):
+        self.level -= 1
+        self.xmlgenerator.characters(multiplystring('\t', self.level))
         self.xmlgenerator.endElement(tag)
-#        self.output.write('</'+tag+'>\n')
+        self.xmlgenerator.characters('\n')
+        self.previous_was_end = True
 
     def element (self, tag, attributes = {}):
-        self.startElement(tag, attributes)
-        self.endElement(tag)
-#        self.output.write ('<'+tag)
-#        if attributes != None:
-#            self.output.write ('\n')
-#            for a in attributes:
-#                self.output.write (a[0] + '="' + a[1] + '"\n')
-#        self.output.write ('/>\n')
+        self.xmlgenerator.characters(multiplystring('\t', self.level))
+        self.xmlgenerator.startElement(tag,  attributes)
+        self.xmlgenerator.endElement(tag)
+        self.xmlgenerator.characters('\n')
+
 
     def message (self, msg):
+        self.xmlgenerator.characters(multiplystring('\t', self.level))
         self.xmlgenerator.characters(msg)
+        self.xmlgenerator.characters('\n')
 #        self.output (encoded_msg = saxutils.escape(msg).encode('UTF-8'))
+
+def multiplystring( string,  times):
+    ret = ''
+    for i in range(0, times):
+        ret += string
+    return ret

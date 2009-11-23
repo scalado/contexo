47bc80b9ee7d31ced85381c3ec82278fd50e2d5c
michalp <michal.pomorski@scalado.com>
2009-11-23 05:37:22 +0100

    fixed a bug in generatig all public header files (possibly used by info)

--- contexo/ctx_depmgr.py
+++ contexo/ctx_depmgr.py
@@ -768,10 +768,10 @@ class CTXDepMgr: # The dependency manager class.
                 pubHeaders = self.cmods[ module_name ].getPubHeaderAbsolutePaths()
 
                 for header in pubHeaders:
-                    if full_path:
-                        header_set.add(self.inputFilePathDict [ header ])
-                    else:
-                        header_set.add ( header )
+                    #if full_path:
+                        #header_set.add(self.inputFilePathDict [ header ])
+                    #else:
+                    header_set.add ( header )
 
                     for dep_header in self.dependencies[header][0]:
                         if (full_path):

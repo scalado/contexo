59a358f278905725e1be41fe5a8290004125166d
Thomas Eriksson <thomas.eriksson@scalado.com>
2010-07-02 17:41:30 +0200

    more stuff

---
+++ contexo/cmdline/git-ctx.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+###############################################################################
+#                                                                             #
+#   git-ctx.py                                                                #
+#   Contexo rspec multi repository tool support for the Git toolsuite         #
+#                                                       (c) Scalado AB 2010   #
+#                                                                             #
+#   Author: Thomas Eriksson (thomas.eriksson@scalado.com)                     #
+#   License GPL v2. See LICENSE.txt.                                          #
+#   ------------                                                              #
+#                                                                             #
+#                                                                             #
+###############################################################################
+# coding=UTF-8
+
+class GITCtx:
+    def __init__( self ):
+
+gitctx = GITCtx

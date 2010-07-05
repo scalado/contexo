c24620a9c1a2cc05002987cf2435a1bcef6e5e7b
Thomas Eriksson <thomas.eriksson@scalado.com>
2010-07-05 11:22:55 +0200

    lunch

--- setup.py
+++ setup.py
@@ -25,7 +25,10 @@ if (oldcontexofiles):
 path = os.path.expandvars("$PATH")
 paths = path.split(os.pathsep)
 
-interestingfiles = ['ctx.py',  'ctx.bat',  'msvc.py', 'msvc.bat',   'tengiltests.py']
+if sys.platform == 'win32':
+	interestingfiles = ['ctx.bat', 'ctx.py',  'git-ctx.py', 'git-ctx.bat', 'msvc.py', 'msvc.bat', 'tengiltests.py']
+else:
+	interestingfiles = ['ctx', 'ctx.py',  'git-ctx', 'git-ctx.py', 'tengiltests.py']
 combination = [(path, file) for path in paths for file in interestingfiles]
 ctxpaths = filter(os.path.exists, map(lambda (pair): os.path.join(*pair), combination) )
 suspected_ctx_paths = filter(lambda ctxpath: re.search(os.path.join('c:', 'program files'), ctxpath, re.IGNORECASE), ctxpaths)
@@ -40,7 +43,7 @@ setup(name="Contexo",
       author="Manuel Astudillo",
       author_email="manuel.astudillo@scalado.com",
       url="http://www.scalado.com",
-      scripts =  ['contexo/cmdline/ctx.py',  'contexo/cmdline/ctx.bat', 'contexo/cmdline/bdef2rspec.py', 'contexo/plugins/export/msvc.py', 'contexo/plugins/export/tengiltests.py' ],
+      scripts =  ['contexo/cmdline/ctx.py', 'contexo/cmdline/ctx.bat', 'contexo/cmdline/git-ctx.py', 'contexo/cmdline/git-ctx', 'contexo/cmdline/ctx', 'contexo/cmdline/bdef2rspec.py', 'contexo/plugins/export/msvc.py', 'contexo/plugins/export/tengiltests.py' ],
       package_dir = { 'contexo.defaults':'defaults' ,'': 'otherlibs',  'contexo':'contexo'},  #the '':'otherlibs' is a hack to make it load argparse without a package, from a subdirectory otherlibs
       package_data={ 'contexo': ['cmdline/ctx.bat'],  'contexo.defaults': ['contexo.cfg', 'bconf/*', 'cdef/*' ], 'contexo.plugins.export': ['msvc.bat','rspectree.bat'] },
       packages = [ 'contexo.defaults', 'contexo' ,  'contexo.platform', 'contexo.plugins', 'contexo.plugins.export' ],

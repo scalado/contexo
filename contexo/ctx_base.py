56a2db603cb4a2f4603820d6a0068beb28727eb0
Thomas Eriksson <thomas.eriksson@scalado.com>
2011-02-09 15:37:27 +0000

    quickfix for TOOLS-31

--- contexo/ctx_base.py
+++ contexo/ctx_base.py
@@ -729,6 +729,12 @@ class CTXBuildSession:
 
         # Expand all commandline mask variables to the corresponding items we prepared.
         cmdline = self.compiler.cdef['LDCOM']
+        # this is a fix for gcc, where compiler arguments may include , which the python config doesn't seem to handle
+        if type(section[ option_name ]) == list and len( section[ option_name ]) > 1:
+            cmdline = (''.join("%s," % (k) for k in self.compiler.cdef['LDCOM']))[0:-1]
+        else:
+            cmdline = self.compiler.cdef['LDCOM']
+
         # Require all mandatory variables in commandline mask
 
         for var in ['%LD', '%SOURCES']:

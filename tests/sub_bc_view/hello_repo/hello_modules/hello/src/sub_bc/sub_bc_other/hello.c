3ca693f1584d111145a9a338696930ae8c441925
Thomas Eriksson <thomas.eriksson@scalado.com>
2011-07-28 09:21:51 +0000

    tests seems to pass, needs to pass bc instead of subBC

--- tests/sub_bc_view/hello_repo/hello_modules/hello/src/sub_bc/sub_bc_other/hello.c
+++ tests/sub_bc_view/hello_repo/hello_modules/hello/src/sub_bc/sub_bc_other/hello.c
@@ -1,7 +1,14 @@
+#ifdef __clang__
+#warning "clang used"
+#else
+#warning "gcc used"
+#error "do not use gcc"
+#endif
+
 #include <stdio.h>
 
 int main() {
 	printf("it works!");
-	exit 0;
+	exit(0);
 }
 

2b3ea4b4cbc227d1dcbc9eadf38bc4ffaf1fca54
Thomas Eriksson <thomas.eriksson@scalado.com>
2012-04-18 11:38:10 +0200

    version bump

--- contexo/ctx_sysinfo.py
+++ contexo/ctx_sysinfo.py
@@ -18,7 +18,7 @@
 
 CTX_VER_MAJOR           = 0
 CTX_VER_MINOR_1         = 17
-CTX_VER_MINOR_2         = 1
+CTX_VER_MINOR_2         = 2
 CTX_VER_STATE           = ''
 CTX_DISPLAYVERSION      = '%d.%d.%d %s'%(CTX_VER_MAJOR, CTX_VER_MINOR_1, CTX_VER_MINOR_2, CTX_VER_STATE)
 CTX_LICENSE             = 'licesed under GPLv2 ( http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt )'
